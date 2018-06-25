f513ffd7d312e9d15e37ec25ac5967d15e107b23
zee <zperezcanals@gmail.com>
2018-06-25 16:20:01 +0800

    loop for cards

--- src/Card.js
+++ src/Card.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Card = ({id, name, email }) =>{
+const Card = ({id, name, username, email }) =>{
 	return (
 		<div className="bg-light-green dib pa3 ma2 grow bw2 shadow-5 tc">
 			<img alt='robot' src={`https://robohash.org/${id}?10x10`} />
 			<div>
 		<h2>{name}</h2>
+			<p>{username}</p>
 				<p>{email}</p>
 			</div>
 		</div>
