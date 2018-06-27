a8e73deb03a0a0a131a4805059cf42703253e41f
zee <zperezcanals@gmail.com>
2018-06-27 14:17:47 +0800

    scrolling component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
+import Scroll from './Scroll'
 import { robots } from './robots';
 import  './App.css';
 
@@ -25,7 +26,9 @@ class App extends Component {
 				<div className='tc'>
 					<h1 className="f1">RoboTeam</h1>
 					<SearchBox  searchChange={this.onSearchChange} />
-					<CardList robots={filteredRobots} />
+					<Scroll>
+						<CardList robots={filteredRobots} />
+					</Scroll>
 				</div>
 		);
 	}
--- src/Card.js
+++ src/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = ({id, name, username, email }) =>{
 	return (
-		<div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
+		<div className="tc grow bg-light-green br3 pa3 ma3 dib bw2 shadow-5">
 			<img alt='robot' src={`https://robohash.org/${id}?200x200`} />
 			<div>
 		<h2>{name}</h2>
